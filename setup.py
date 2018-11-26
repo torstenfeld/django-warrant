cd15d1a3e5cab9717ee398fe81fcf8e84e236965
Torsten Feld <torsten@torsten-feld.de>
2018-11-26 15:05:50 +0100

    made setup.py pip 10+ compatible

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import os
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip < 10
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 
 version = '0.1.0'
+# def parse_requirements(filename):
+#     """ load requirements from a pip requirements file """
+#     lineiter = (line.strip() for line in open(filename))
+#     return [line for line in lineiter if line and not line.startswith("#")]
 
 README="""Django library that uses the warrant python utility library to provide authentication via AWS Cognito."""
 
